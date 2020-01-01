GerHobbelt/libpipeline

commit eceb4fc9c1a800739f3876a5c9538376adc77876

Use _exit rather than exit in pipecmd_exec
This avoids bugs such as functions registered using atexit being called
multiple times.

* lib/pipeline.c (pipecmd_exec): Call _exit rather than exit.
* NEWS: Document this.

lib/pipeline.c
@@ -820,7 +820,7 @@ void pipecmd_exec (pipecmd *cmd)
 				cmdf->free_func (cmdf->data);
 			if (cmd->pre_exec_free_func)
 				cmd->pre_exec_free_func (cmd->pre_exec_data);
-			exit (0);
+			_exit (0);
 		}
 
 		case PIPECMD_SEQUENCE: {
@@ -897,12 +897,12 @@ void pipecmd_exec (pipecmd *cmd)
 
 				if (WIFSIGNALED (status)) {
 					raise (WTERMSIG (status));
-					exit (1); /* just to make sure */
+					_exit (1); /* just to make sure */
 				} else if (status && WIFEXITED (status))
-					exit (WEXITSTATUS (status));
+					_exit (WEXITSTATUS (status));
 			}
 
-			exit (0);
+			_exit (0);
 		}
 	}
 
@@ -910,7 +910,7 @@ void pipecmd_exec (pipecmd *cmd)
 	/* Never called, but gcc doesn't realise that error with non-zero
 	 * status always exits.
 	 */
-	exit (EXEC_FAILED_EXIT_STATUS);
+	_exit (EXEC_FAILED_EXIT_STATUS);
 }
 
 void pipecmd_free (pipecmd *cmd)

commit e8b81ae41835ec163d16c5b23ea6a54e53b2555a

pipecmd_exec: Flush stdio in PIPECMD_FUNCTION case
This fixes a regression in eceb4fc9c1a800739f3876a5c9538376adc77876
where we could lose some output from functions that write to standard
output but don't explicitly flush.

* lib/pipeline.c (pipecmd_exec): Flush output streams before exiting in
the PIPECMD_FUNCTION case.

lib/pipeline.c
@@ -820,6 +820,7 @@ void pipecmd_exec (pipecmd *cmd)
 				cmdf->free_func (cmdf->data);
 			if (cmd->pre_exec_free_func)
 				cmd->pre_exec_free_func (cmd->pre_exec_data);
+			fflush (NULL);
 			_exit (0);
 		}
 